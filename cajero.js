38e10452f8ad4c9e1743778feb47a1be595e3a45
ricardoescobarangel <ricardoescobarangel@gmail.com>
2022-11-15 19:44:34 -0500

    continuación trabajo en modal (balance y depósitos)

--- cajero.js
+++ cajero.js
@@ -26,12 +26,13 @@ backdrop.addEventListener("click", function(){
 })
 
 
-// Revisar con el sensei...
 document.addEventListener("DOMContentLoaded", function(){
     const usuarioActual = JSON.parse(window.sessionStorage.getItem("usuarioActual"))
     console.log(usuarioActual.nombre)
     saludo.innerHTML = `¡Bienvenido, ${usuarioActual.nombre}!`
 })
+
+// Revisar con el sensei porque da valor 'undefined'
 
 const consultaBalance = document.querySelector("#balance")
 consultaBalance.addEventListener("click", function(){
@@ -44,4 +45,20 @@ consultaBalance.addEventListener("click", function(){
 
     cuerpoModal.innerHTML = `<h3 style="font-size: 2rem; height: 25vh; display: flex; align-items: center">$ ${balance}</h3>`
     tituloModal.innerHTML = `Apenas tienes:`
+})
+
+const botonDeposito = document.querySelector("#depo")
+botonDeposito.addEventListener("click", function (){
+    abrirModal()
+
+    const cuerpoModal = document.querySelector("#modal div")
+    const tituloModal = document.querySelector("#modal h5")
+
+    cuerpoModal.innerHTML = `
+    <form style= "height: 40vh; width: 60vw; display: flex; flex-direction: column; justify-content: center; align-items: center">
+      <input id="nuevoDeposito" style= "width: 40vw; height: 2rem; text-align: center; border: none; border-radius: 999px" placeholder="¿Cuánto me vas a entregar?" type="number"/>
+      <button id="confirmasDeposito" class="boton" >dámelo</button>    
+    </form>
+   `
+   tituloModal.innerHTML = 'Deposito a cuenta'
 })
